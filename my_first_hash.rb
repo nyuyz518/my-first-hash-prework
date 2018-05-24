16d769a40cee9bd6b257af9b8bc304fbc532dd50
Lillian Zhang <lilyzhangyun@gmail.com>
2018-05-24 15:01:44 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,8 +14,9 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+  shipping_manifest["oil paintings"]
 end
-shipping_manifest["oil paintings"]
+
 
 
 def adding
